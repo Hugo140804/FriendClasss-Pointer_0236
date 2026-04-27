843369ab6de26af1f69a060b4b107938860d3814
Hugo140804 <hgugi3nsn@gmail.com>
2026-04-27 20:06:18 +0700

    Metode input

--- TUGAS.cpp
+++ TUGAS.cpp
@@ -8,5 +8,14 @@ private:
     float diagonal1, diagonal2, sisi;
  
 public:
+        void input() {
+        cout << "=== Input Belah Ketupat ===" << endl;
+        cout << "Masukkan diagonal 1 : ";
+        cin >> diagonal1;
+        cout << "Masukkan diagonal 2 : ";
+        cin >> diagonal2;
+        cout << "Masukkan panjang sisi : ";
+        cin >> sisi;
+    }
 
 
